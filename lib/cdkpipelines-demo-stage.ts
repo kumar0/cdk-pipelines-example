e2f6f066b415be4ef3febf881132b8d6c286c5da
pikumar0 <harish.kumar@skillmetrics.io>
2021-10-19 23:53:47 +0100

    name removed

--- lib/cdkpipelines-demo-stage.ts
+++ lib/cdkpipelines-demo-stage.ts
@@ -4,15 +4,15 @@ import { CdkpipelinesDemoStack } from './cdk-pipelines-demo-stack';
 /**
  * Deployable unit of web service app
  */
- export class CdkpipelinesDemoStage extends Stage {
-    public readonly urlOutput: CfnOutput;
-    
-    constructor(scope: Construct, id: string, props?: StageProps) {
-      super(scope, id, props);
-  
-      const service = new CdkpipelinesDemoStack(this, 'WebService');
-      
-      // Expose CdkpipelinesDemoStack's output one level higher
-      this.urlOutput = service.urlOutput;
-    }
+export class CdkpipelinesDemoStage extends Stage {
+  public readonly urlOutput: CfnOutput;
+
+  constructor(scope: Construct, id: string, props?: StageProps) {
+    super(scope, id, props);
+
+    const service = new CdkpipelinesDemoStack(this, 'WebService');
+
+    // Expose CdkpipelinesDemoStack's output one level higher
+    this.urlOutput = service.urlOutput;
   }
+}
