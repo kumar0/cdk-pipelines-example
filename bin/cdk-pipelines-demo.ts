f3fc92f4017d32870347a3bfddca3af5a8a16a75
pikumar0 <harish.kumar@skillmetrics.io>
2021-10-21 10:06:59 +0100

    PS-1  Another Test Commit

--- bin/cdk-pipelines-demo.ts
+++ bin/cdk-pipelines-demo.ts
@@ -5,7 +5,7 @@ import { CdkpipelinesDemoPipelineStack } from '../lib/cdkpipelines-demo-pipeline
 
 const app = new cdk.App();
 new CdkpipelinesDemoPipelineStack(app, 'CdkpipelinesDemoPipelineStack', {
-  // env: { account: 'ACCOUNTNUMBER', region: 'REGION' },
+  // env: { account : 'ACCOUNTNUMBER', region: 'REGION' },
 });
 
 app.synth();
